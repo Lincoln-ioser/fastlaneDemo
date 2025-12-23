3b08973ad8b54d3f7add60b88cbd41afe56afde6
kangkang018 <lingkang@qfast.ai>
2025-12-23 16:35:50 +0800

    初始化项目

---
+++ FastlaneDemo/FastlaneDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastlaneDemoApp.swift
+//  FastlaneDemo
+//
+//  Created by kong lingkang on 2025/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FastlaneDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
